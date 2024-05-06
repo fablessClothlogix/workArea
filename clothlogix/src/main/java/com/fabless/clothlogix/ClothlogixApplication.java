e903fee703352ae98517306266cbbda92f3bf19e
emersonC787 <e.carvalho@fabless.it>
2024-05-06 12:44:42 +0200

    Add files via upload

---
+++ clothlogix/src/main/java/com/fabless/clothlogix/ClothlogixApplication.java
@@ -0,0 +1,13 @@
+package com.fabless.clothlogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothlogixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothlogixApplication.class, args);
+	}
+
+}
