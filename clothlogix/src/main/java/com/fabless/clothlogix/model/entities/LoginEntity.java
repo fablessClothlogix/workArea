93e407324000d866eb6e7fe67eda28296dee7ad3
emesb <e.carvalho@fabless.it>
2024-05-09 15:02:09 +0200

    commit 1.0.0

---
+++ clothlogix/src/main/java/com/fabless/clothlogix/model/entities/LoginEntity.java
@@ -0,0 +1,28 @@
+package com.fabless.clothlogix.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "login")
+public class LoginEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private Integer id;
+    @Column(name = "username")
+    private String username;
+    @Column (name = "password")
+    private String password;
+
+    public LoginEntity(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ clothlogix/src/main/java/com/fabless/clothlogix/model/entities/UtenteEntity.java
@@ -0,0 +1,65 @@
+package com.fabless.clothlogix.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "utente")
+public class UtenteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cognome")
+    private String cognome;
+
+    @Column(name = "data")
+    private Date dataDiNascita;
+
+    @Column(name = "indirizzo")
+    private String indirizzo;
+
+    @Column(name = "citta")
+    private String citta;
+
+    @Column(name = "regione")
+    private String regione;
+
+    @Column(name = "cap")
+    private String cap;
+
+    @Column(name = "nazionalita")
+    private String nazionalita;
+
+    @OneToOne
+    @JoinColumn(name = "id_utente")
+    @Column(name = "login")
+    private LoginEntity login;
+
+    public UtenteEntity() {
+    }
+
+    public UtenteEntity(Integer id, String nome, String cognome, Date dataDiNascita, String indirizzo, String citta, String regione, String cap, String nazionalita, LoginEntity login) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataDiNascita = dataDiNascita;
+        this.indirizzo = indirizzo;
+        this.citta = citta;
+        this.regione = regione;
+        this.cap = cap;
+        this.nazionalita = nazionalita;
+        this.login = login;
+    }
+}
